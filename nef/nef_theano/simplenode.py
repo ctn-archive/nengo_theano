6ebd196535b54afa120e8462ad5a3647ea2c1beb
Trevor Bekolay <tbekolay@gmail.com>
2013-03-19 15:40:02 -0400

    Made it easier to import from nef_theano.

--- nef/nef_theano/simplenode.py
+++ nef/nef_theano/simplenode.py
@@ -6,9 +6,9 @@ import theano
 
 from . import origin
 
-class SimpleNode():
+class SimpleNode(object):
     """A SimpleNode allows you to put arbitary code as part of an NEF model.
-        
+
     This object has Origins and Terminations which can be used just like
     any other Nengo component. Arbitrary code can be run every time step,
     making this useful for simulating sensory systems (reading data
@@ -16,32 +16,32 @@ class SimpleNode():
     a file or driving a robot, for example), or even parts of the brain
     that we don't want a full neural model for (symbolic reasoning or
     declarative memory, for example).
-    
+
     You can have as many origins you like.  The dimensionality
-    of the origins are set by the length of the returned vector of floats.  
-    
+    of the origins are set by the length of the returned vector of floats.
+
       class SquaringFiveValues(nef.SimpleNode):
           def init(self):
               self.value=0
           def origin_output(self):
               return [self.value]
-    
+
     There is also a special method called tick() that is called once per
     time step.
 
       class HelloNode(nef.SimpleNode):
           def tick(self):
               print 'Hello world'
-              
+
     The current time can be accessed via `self.t`.  This value will be the
     time for the beginning of the current time step.  The end of the current
     time step is `self.t_end`.
-    
+
     """
 
     def __init__(self, name):
         """
-        :param string name: the name of the created node          
+        :param string name: the name of the created node
         :param float pstc: the default time constant on the filtered inputs
         :param int dimensions:
             the number of dimensions of the decoded input signal
@@ -62,43 +62,43 @@ class SimpleNode():
 
     def init(self):
         """Initialize the node.
-        
+
         Override this to initialize any internal variables. This will
         also be called whenever the simulation is reset.
-        
-        """        
+
+        """
         pass
 
     def tick(self):
         """An extra utility function that is called every time step.
-        
+
         Override this to create custom behaviour that isn't necessarily tied
         to a particular input or output. Often used to write spike data
         to a file or produce some other sort of custom effect.
-        
+
         """
         pass
 
     def reset(self, **kwargs):
         """Reset the state of all the internal variables."""
-        self.init(**kwargs)    
+        self.init(**kwargs)
 
     def theano_tick(self):
         """Run the simple node.
 
-        :param float start: The time to start running 
+        :param float start: The time to start running
         :param float end: The time to stop running
 
         """
-        self.tick()    
+        self.tick()
 
         for origin in self.origin.values():
             value = origin.func()
 
             # if value is a scalar output, make it a list
             if isinstance(value, Number):
-                value = [value] 
+                value = [value]
 
             # cast as float32 for consistency / speed,
             # but _after_ it's been made a list
-            origin.decoded_output.set_value(np.float32(value)) 
+            origin.decoded_output.set_value(np.float32(value))
