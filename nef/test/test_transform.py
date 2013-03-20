d8fc3120dd57f02a54e1374014dd3c34f3c71d30
studywolf <travis.dewolf@gmail.com>
2013-03-20 13:57:26 -0700

    Transform for decoded-decoded handles for network arrays
    - updated transform test to include check for this too

--- nef/test/test_transform.py
+++ nef/test/test_transform.py
@@ -13,11 +13,13 @@ def func(x):
 
 net = nef.Network('Transform Test')
 net.make_input('in', value=func)
-net.make('A', 300, 3)
+net.make('A', neurons=300, dimensions=3)
+net.make('B', neurons=300, array_size=3, dimensions=1)
 
 # define our transform and connect up! 
 transform = [[0, 1], [1, 0], [1, -1]]
 net.connect('in', 'A', transform=transform)
+net.connect('in', 'B', transform=transform)
 
 timesteps = 500
 dt_step = 0.01
@@ -27,6 +29,8 @@ Ip = net.make_probe(net.nodes['in'].origin['X'].decoded_output,
                     dt_sample=dt_step, pstc=pstc)
 Ap = net.make_probe(net.nodes['A'].origin['X'].decoded_output,
                     dt_sample=dt_step, pstc=pstc)
+Bp = net.make_probe(net.nodes['B'].origin['X'].decoded_output,
+                    dt_sample=dt_step, pstc=pstc)
 
 print "starting simulation"
 net.run(timesteps * dt_step)
@@ -34,11 +38,17 @@ net.run(timesteps * dt_step)
 plt.ioff(); plt.clf(); 
 plt.subplot(411); plt.title('Input')
 plt.plot(Ip.get_data()); plt.legend(['In(0)','In(1)'])
-plt.subplot(412); plt.title('A(0) = In(1)')
+plt.subplot(412); plt.title('{A,B}(0) = In(1)')
 plt.plot(Ap.get_data()[:,0])
-plt.subplot(413); plt.title('A(1) = In(0)')
+plt.plot(Bp.get_data()[:,0])
+plt.legend(['A','B'])
+plt.subplot(413); plt.title('{A,B}(1) = In(0)')
 plt.plot(Ap.get_data()[:,1])
-plt.subplot(414); plt.title('A(2) = In(0) - In(1)')
+plt.plot(Bp.get_data()[:,1])
+plt.legend(['A','B'])
+plt.subplot(414); plt.title('{A,B}(2) = In(0) - In(1)')
 plt.plot(Ap.get_data()[:,2])
+plt.plot(Bp.get_data()[:,2])
+plt.legend(['A','B'])
 plt.tight_layout()
 plt.show()
