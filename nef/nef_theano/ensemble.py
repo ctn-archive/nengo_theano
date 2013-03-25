743dcacddfde8506d61e56c388d0be01438dd855
studywolf <travis.dewolf@gmail.com>
2013-03-24 17:30:35 -0700

    Working for basic n_array pre population
    basic test with pre population n_array, d = 1, array_size = 2
    TODO:
    	- lots and lots of testing for different pre/post/error
              dim/array_size combinations

--- nef/nef_theano/ensemble.py
+++ nef/nef_theano/ensemble.py
@@ -324,14 +324,23 @@ class Ensemble:
 
         learned_term = learned_termination_class(
             pre, self, error, weight_matrix)
-        learn_output = [TT.dot(
-            TT.reshape(pre.neurons.output[0], (1, pre.neurons_num)), 
-            learned_term.weight_matrix[i]) for i in range(self.array_size * pre.array_size)]
+
+        # np.ceil((i + 1) / float(self.post.array_size)) - 1 generates 0 post.array_size times, 
+        # then 1 post.array_size times, then 2 post.array_size times, etc
+        # so with pre.array_size = post.array_size = 2 
+        # we're connecting it up in order pre[0]-post[0], pre[0]-post[1], pre[1]-post[0], pre[1]-post[1]
+        learn_projections = [TT.dot(
+            pre.neurons.output[int(np.ceil((i + 1) / float(self.array_size)) - 1)],  
+            learned_term.weight_matrix[i % self.array_size]) for i in range(self.array_size * pre.array_size)]
+        # now want to sum all the output to each of the post ensembles 
+        # going to reshape and sum along the 0 axis
+        learn_output = TT.sum( TT.reshape( learn_projections, (pre.array_size, self.array_size, self.neurons_num) ), axis=0)
         # learn_output should now be (array_size x neurons_num x 1)
         # reshape to make it (array_size x neurons_num)
-        print 'learn_output', learn_output
+        print 'learn_output.eval().shape', learn_output[0].eval().shape
         learn_output = TT.reshape( learn_output, (self.array_size, 
                                    self.neurons_num) )
+        print 'learn_output', learn_output
 
         # add learn output to the accumulator to handle
         # the input_current from this connection during simulation
