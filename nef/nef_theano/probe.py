f3c67cc8d553572ba1af9c8bc5b93bed213d234f
hunse <hunse@ctn>
2013-03-28 12:41:24 -0400

    Added Filter class to easily filter data during simulation. - the Probe class now uses this filtering - TODO: modify Accumulator and hPESTermination to use this filter

--- nef/nef_theano/probe.py
+++ nef/nef_theano/probe.py
@@ -4,6 +4,8 @@ import numpy as np
 import theano
 import theano.tensor as TT
 
+from .filter import Filter
+
 class Probe(object):
     """A class to record from things (i.e., origins).
 
@@ -13,33 +15,22 @@ class Probe(object):
     def __init__(self, name, network, target, dt_sample, pstc=0.03):
         self.name = name
         self.target = target
-        self.i = -1
-        self.t = 0
-        self.dt = network.dt
+        # self.t = 0
         self.dt_sample = dt_sample
-        self.pstc = pstc
 
-        target_value = np.zeros_like(target.get_value())
-        self.data = np.zeros((self.buffer_size,) + target_value.shape)
+        # create array to store the data over many time steps
+        self.data = np.zeros((self.buffer_size,) + target.get_value().shape)
+        self.i = -1 # index of the last sample taken
 
-        self.filtered_data = (
-            theano.shared(target_value, name='probe.filtered_data')
-            if self.pstc > 0 else None)
+        # create a filter to filter the data
+        self.filter = Filter(network.dt, pstc, source=target)
 
     def update(self):
-        if self.filtered_data is not None:
-            alpha = TT.cast(self.dt/self.pstc , dtype='float32')
-            filtered_value = self.filtered_data + alpha * (
-                self.target - self.filtered_data)
-            return collections.OrderedDict({
-                    self.filtered_data: filtered_value})
-        else:
-            return collections.OrderedDict()
+        return self.filter.update()
 
     def theano_tick(self):
         i_samp = int(np.floor(self.t / self.dt_sample))
         if i_samp > self.i:
-            self.i = i_samp
             # we're as close to a sample point as we're going to get,
             # so take a sample
             if i_samp >= len(self.data):
@@ -48,10 +39,9 @@ class Probe(object):
                     [self.data, np.zeros((self.buffer_size,)
                                          + self.data.shape[1:])])
 
-            if self.filtered_data is not None:
-                self.data[i_samp] = self.filtered_data.get_value()
-            else:
-                self.data[i_samp] = self.target.get_value()
+            # record the filtered value
+            self.data[i_samp] = self.filter.value.get_value()
+            self.i = i_samp
 
     def get_data(self):
         return self.data[:self.i+1]
