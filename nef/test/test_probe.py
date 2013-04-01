b7f0da2a2160aba013b6450f7290c57b703e183d
travis dewolf <travis.dewolf@gmail.com>
2013-04-01 19:30:42 -0400

    Reworked write out from network
    - renamed net.write_data to net.write_data_to_hd5
    - put neo imports inside this function
    - removed net.read_data
    - put reading plotting test inside test_writeout

--- nef/test/test_probe.py
+++ nef/test/test_probe.py
@@ -5,8 +5,11 @@ and write to file.
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import time
 
 from .. import nef_theano as nef
+
+build_time_start = time.time()
 
 net = nef.Network('Probe Test')
 net.make_input('in', math.sin)
@@ -16,7 +19,7 @@ net.make('B', 5, 1)
 net.connect('in', 'A')
 net.connect('in', 'B')
 
-timesteps = 500
+timesteps = 70000
 dt_step = 0.01
 t = np.linspace(dt_step, timesteps*dt_step, timesteps)
 pstc = 0.01
@@ -26,8 +29,14 @@ Ap = net.make_probe('A', dt_sample=dt_step, pstc=pstc)
 Bp = net.make_probe('B', dt_sample=dt_step)
 BpSpikes = net.make_probe('B', data_type='spikes', dt_sample=dt_step)
 
+build_time_end = time.time()
+
 print "starting simulation"
 net.run(timesteps*dt_step)
+
+sim_time_end = time.time()
+print "\nBuild time: %0.10fs" % (build_time_end - build_time_start)
+print "Sim time: %0.10fs" % (sim_time_end - build_time_end)
 
 # plot the results
 plt.ioff(); plt.close(); 
