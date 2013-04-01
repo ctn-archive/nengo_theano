22e0f7f2d0068d0193757f4ba48c51c87899f484
travis dewolf <travis.dewolf@gmail.com>
2013-04-01 10:15:54 -0400

    Reworked probe to use lists
    Cuts down on the overhead of our own code, doing the same
    array size management that list already does.
    
    -get_data still returns a numpy array

--- nef/nef_theano/probe.py
+++ nef/nef_theano/probe.py
@@ -1,4 +1,5 @@
 import collections
+import neo
 
 import numpy as np
 import theano
@@ -8,40 +9,44 @@ from .filter import Filter
 
 class Probe(object):
     """A class to record from things (i.e., origins).
-
     """
-    buffer_size = 1000
-
-    def __init__(self, name, network, target, dt_sample, pstc=0.03):
+    def __init__(self, name, network, target, target_name, dt_sample, pstc=0.03):
+        """
+        :param string name:
+        :param Network network:
+        :param target:
+        :type target: theano object
+        :param float dt_sample:
+        :param float pstc:
+        """
         self.name = name
         self.target = target
-        # self.t = 0
+        self.target_name = target_name
         self.dt_sample = dt_sample
 
         # create array to store the data over many time steps
-        self.data = np.zeros((self.buffer_size,) + target.get_value().shape)
+        self.data = [] #np.zeros((self.buffer_size,) + target.get_value().shape)
         self.i = -1 # index of the last sample taken
 
         # create a filter to filter the data
         self.filter = Filter(network.dt, pstc, source=target)
 
     def update(self):
+        """
+        """
         return self.filter.update()
 
     def theano_tick(self):
+        """
+        """
         i_samp = int(np.floor(self.t / self.dt_sample))
         if i_samp > self.i:
             # we're as close to a sample point as we're going to get,
             # so take a sample
-            if i_samp >= len(self.data):
-                # increase the buffer
-                self.data = np.vstack(
-                    [self.data, np.zeros((self.buffer_size,)
-                                         + self.data.shape[1:])])
-
-            # record the filtered value
-            self.data[i_samp] = self.filter.value.get_value()
+            self.data.append(self.filter.value.get_value()) 
             self.i = i_samp
 
     def get_data(self):
-        return self.data[:self.i+1]
+        """Return the collected data as numpy array
+        """
+        return np.array(self.data[:self.i+1])
