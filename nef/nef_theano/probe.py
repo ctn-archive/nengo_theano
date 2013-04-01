545723ba02ba2869ad2f46de87dca7317104b79a
travis dewolf <travis.dewolf@gmail.com>
2013-04-01 14:13:55 -0400

    Basic file IO for hdf5 added to network
    Really basic. There is a net.write_data and net.read_data.
    You can't do anything once you read it, but some of it is plotted.
    TODO: lots, probably one of the first is making a logger class for these
    
    - also changed probe back to using numpy arrays to keep things clean with
      the theano interactions

--- nef/nef_theano/probe.py
+++ nef/nef_theano/probe.py
@@ -2,50 +2,50 @@ import collections
 
 import numpy as np
 import theano
+import collections
+
+import numpy as np
+import theano
 import theano.tensor as TT
 
 from .filter import Filter
 
 class Probe(object):
     """A class to record from things (i.e., origins).
+
     """
+    buffer_size = 1000
+
     def __init__(self, name, network, target, target_name, dt_sample, pstc=0.03):
-        """
-        :param string name:
-        :param Network network:
-        :param target:
-        :type target: theano object
-        :param float dt_sample:
-        :param float pstc:
-        """
         self.name = name
         self.target = target
         self.target_name = target_name
         self.dt_sample = dt_sample
 
         # create array to store the data over many time steps
-        self.data = [] #np.zeros((self.buffer_size,) + target.get_value().shape)
+        self.data = np.zeros((self.buffer_size,) + target.get_value().shape)
         self.i = -1 # index of the last sample taken
 
         # create a filter to filter the data
         self.filter = Filter(network.dt, pstc, source=target)
 
     def update(self):
-        """
-        """
         return self.filter.update()
 
     def theano_tick(self):
-        """
-        """
         i_samp = int(np.floor(self.t / self.dt_sample))
         if i_samp > self.i:
             # we're as close to a sample point as we're going to get,
             # so take a sample
-            self.data.append(self.filter.value.get_value()) 
+            if i_samp >= len(self.data):
+                # increase the buffer
+                self.data = np.vstack(
+                    [self.data, np.zeros((self.buffer_size,)
+                                         + self.data.shape[1:])])
+
+            # record the filtered value
+            self.data[i_samp] = self.filter.value.get_value()
             self.i = i_samp
 
     def get_data(self):
-        """Return the collected data as numpy array
-        """
-        return np.array(self.data[:self.i+1])
+        return self.data[:self.i+1]
