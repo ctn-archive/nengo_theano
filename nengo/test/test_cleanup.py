00b3e779b14518c54659460d03bb008a5cf7161e
James Bergstra <james.bergstra@gmail.com>
2013-05-09 17:37:02 -0400

    moving test scripts to test functions

--- nengo/test/test_cleanup.py
+++ nengo/test/test_cleanup.py
@@ -1,48 +1,53 @@
+import nose
 import numpy as np
 
 from .. import nef_theano as nef
 
-net = nef.Network('Cleanup',seed=3)
 
-D = 128
-M = 50000
-N1 = 100
-N2 = 50
-index = 0
+def test_cleanup():
+    raise nose.SkipTest()
 
-def make_vector():
-    v = np.random.normal(size=(D,))
-    
-    norm = np.linalg.norm(v)
-    v = v / norm
-    return v
+    net = nef.Network('Cleanup',seed=3)
 
-print 'making words...'
-words = [make_vector() for i in range(M)]
-words = np.array(words)
-print '...done'
+    D = 128
+    M = 50000
+    N1 = 100
+    N2 = 50
+    index = 0
 
-net.make_array('A', N1, D)
-net.make_array('B', N1, D)
+    def make_vector():
+        v = np.random.normal(size=(D,))
+        
+        norm = np.linalg.norm(v)
+        v = v / norm
+        return v
 
-net.make_array('C', N2, M)#,intercept=(0.6,0.9))
-print 'made'
+    print 'making words...'
+    words = [make_vector() for i in range(M)]
+    words = np.array(words)
+    print '...done'
 
-net.connect('A', 'C', words.T, pstc=0.1)
-net.connect('C', 'B', words, pstc=0.1)
+    net.make_array('A', N1, D)
+    net.make_array('B', N1, D)
 
-net.make_input('input', words[index])
-net.connect('input', 'A', pstc=0.1)
+    net.make_array('C', N2, M)#,intercept=(0.6,0.9))
+    print 'made'
 
-net.run(0.001)
+    net.connect('A', 'C', words.T, pstc=0.1)
+    net.connect('C', 'B', words, pstc=0.1)
 
-import time
-start = time.time()
-
-for i in range(5000):
-    #print i,net.ensemble['A'].origin['X'].value.get_value()
-    print i, words[index, :4],
-    print net.nodes['C'].accumulator[0.1].projected_value.get_value()[:4]
+    net.make_input('input', words[index])
+    net.connect('input', 'A', pstc=0.1)
 
     net.run(0.001)
-    print (time.time() - start) / (i + 1)
+
+    import time
+    start = time.time()
+
+    for i in range(5000):
+        #print i,net.ensemble['A'].origin['X'].value.get_value()
+        print i, words[index, :4],
+        print net.nodes['C'].accumulator[0.1].projected_value.get_value()[:4]
+
+        net.run(0.001)
+        print (time.time() - start) / (i + 1)
--- nengo/test/test_probe.py
+++ nengo/test/test_probe.py
@@ -9,44 +9,46 @@ import time
 
 from .. import nef_theano as nef
 
-build_time_start = time.time()
+def test_probe():
 
-net = nef.Network('Probe Test')
-net.make_input('in', math.sin)
-net.make('A', 50, 1)
-net.make('B', 5, 1)
+    build_time_start = time.time()
 
-net.connect('in', 'A')
-net.connect('in', 'B')
+    net = nef.Network('Probe Test')
+    net.make_input('in', math.sin)
+    net.make('A', 50, 1)
+    net.make('B', 5, 1)
 
-timesteps = 100
-dt_step = 0.01
-t = np.linspace(dt_step, timesteps*dt_step, timesteps)
-pstc = 0.01
+    net.connect('in', 'A')
+    net.connect('in', 'B')
 
-Ip = net.make_probe('in', dt_sample=dt_step, pstc=pstc)
-Ap = net.make_probe('A', dt_sample=dt_step, pstc=pstc)
-Bp = net.make_probe('B', dt_sample=dt_step)
-BpSpikes = net.make_probe('B', data_type='spikes', dt_sample=dt_step)
+    timesteps = 100
+    dt_step = 0.01
+    t = np.linspace(dt_step, timesteps*dt_step, timesteps)
+    pstc = 0.01
 
-build_time_end = time.time()
+    Ip = net.make_probe('in', dt_sample=dt_step, pstc=pstc)
+    Ap = net.make_probe('A', dt_sample=dt_step, pstc=pstc)
+    Bp = net.make_probe('B', dt_sample=dt_step)
+    BpSpikes = net.make_probe('B', data_type='spikes', dt_sample=dt_step)
 
-print "starting simulation"
-net.run(timesteps*dt_step)
+    build_time_end = time.time()
 
-sim_time_end = time.time()
-print "\nBuild time: %0.10fs" % (build_time_end - build_time_start)
-print "Sim time: %0.10fs" % (sim_time_end - build_time_end)
+    print "starting simulation"
+    net.run(timesteps*dt_step)
 
-# plot the results
-plt.ioff(); plt.close(); 
-plt.subplot(3,1,1)
-plt.plot(t, Ip.get_data(), 'x'); plt.title('Input')
-plt.subplot(3,1,2)
-plt.plot(Ap.get_data()); plt.title('A')
-plt.subplot(3,1,3); plt.hold(1)
-plt.plot(Bp.get_data())
-for row in BpSpikes.get_data().T: 
-    plt.plot(row[0]); 
-plt.title('B')
-plt.show()
+    sim_time_end = time.time()
+    print "\nBuild time: %0.10fs" % (build_time_end - build_time_start)
+    print "Sim time: %0.10fs" % (sim_time_end - build_time_end)
+
+    # plot the results
+    plt.ioff(); plt.close(); 
+    plt.subplot(3,1,1)
+    plt.plot(t, Ip.get_data(), 'x'); plt.title('Input')
+    plt.subplot(3,1,2)
+    plt.plot(Ap.get_data()); plt.title('A')
+    plt.subplot(3,1,3); plt.hold(1)
+    plt.plot(Bp.get_data())
+    for row in BpSpikes.get_data().T: 
+        plt.plot(row[0]); 
+    plt.title('B')
+    plt.show()
